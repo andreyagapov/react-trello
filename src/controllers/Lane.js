453ab787a3d78ff1704c86ba8fff30da4d07296e
Andrey Agapov <ana@appfellas.com>
2019-12-06 13:47:03 +0200

    always showing lane footer

--- src/controllers/Lane.js
+++ src/controllers/Lane.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
-import {bindActionCreators} from 'redux'
-import {connect} from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import isEqual from 'lodash/isEqual'
 import cloneDeep from 'lodash/cloneDeep'
 import pick from 'lodash/pick'
@@ -25,10 +25,10 @@ class Lane extends Component {
   handleScroll = evt => {
     const node = evt.target
     const elemScrolPosition = node.scrollHeight - node.scrollTop - node.clientHeight
-    const {onLaneScroll} = this.props
+    const { onLaneScroll } = this.props
     if (elemScrolPosition <= 0 && onLaneScroll && !this.state.loading) {
-      const {currentPage} = this.state
-      this.setState({loading: true})
+      const { currentPage } = this.state
+      this.setState({ loading: true })
       const nextPage = currentPage + 1
       onLaneScroll(nextPage, this.props.id).then(moreCards => {
         if (!moreCards || moreCards.length === 0) {
@@ -41,7 +41,7 @@ class Lane extends Component {
             nextPage: nextPage
           })
         }
-        this.setState({loading: false})
+        this.setState({ loading: false })
       })
     }
   }
@@ -49,7 +49,7 @@ class Lane extends Component {
   sortCards(cards, sortFunction) {
     if (!cards) return []
     if (!sortFunction) return cards
-    return cards.concat().sort(function(card1, card2) {
+    return cards.concat().sort(function (card1, card2) {
       return sortFunction(card1, card2)
     })
   }
@@ -70,34 +70,34 @@ class Lane extends Component {
 
   removeCard = cardId => {
     this.props.onCardDelete && this.props.onCardDelete(cardId, this.props.id)
-    this.props.actions.removeCard({laneId: this.props.id, cardId: cardId})
+    this.props.actions.removeCard({ laneId: this.props.id, cardId: cardId })
   }
 
   handleCardClick = (e, card) => {
-    const {onCardClick} = this.props
+    const { onCardClick } = this.props
     onCardClick && onCardClick(card.id, card.metadata, card.laneId)
     e.stopPropagation()
   }
 
   showEditableCard = () => {
-    this.setState({addCardMode: true})
+    this.setState({ addCardMode: true })
   }
 
   hideEditableCard = () => {
-    this.setState({addCardMode: false})
+    this.setState({ addCardMode: false })
   }
 
   addNewCard = params => {
     const laneId = this.props.id
     const id = uuidv1()
     this.hideEditableCard()
-    let card = {id, ...params}
-    this.props.actions.addCard({laneId, card})
+    let card = { id, ...params }
+    this.props.actions.addCard({ laneId, card })
     this.props.onCardAdd(card, laneId)
   }
 
-  onDragStart = ({payload}) => {
-    const {handleDragStart} = this.props
+  onDragStart = ({ payload }) => {
+    const { handleDragStart } = this.props
     handleDragStart && handleDragStart(payload.id, payload.laneId)
   }
 
@@ -106,16 +106,16 @@ class Lane extends Component {
   }
 
   get groupName() {
-    const {boardId} = this.props
+    const { boardId } = this.props
     return `TrelloBoard${boardId}Lane`
   }
 
   onDragEnd = (laneId, result) => {
-    const {handleDragEnd} = this.props
-    const {addedIndex, payload} = result
-    this.setState({isDraggingOver: false})
+    const { handleDragEnd } = this.props
+    const { addedIndex, payload } = result
+    this.setState({ isDraggingOver: false })
     if (addedIndex != null) {
-      const newCard = {...cloneDeep(payload), laneId}
+      const newCard = { ...cloneDeep(payload), laneId }
       const response = handleDragEnd ? handleDragEnd(payload.id, payload.laneId, laneId, addedIndex, newCard) : true
       if (response === undefined || !!response) {
         this.props.actions.moveCardAcrossLanes({
@@ -144,7 +144,7 @@ class Lane extends Component {
       components,
       t
     } = this.props
-    const {addCardMode, collapsed} = this.state
+    const { addCardMode, collapsed } = this.state
 
     const showableCards = collapsed ? [] : cards
 
@@ -167,8 +167,8 @@ class Lane extends Component {
       return cardDraggable && (!card.hasOwnProperty('draggable') || card.draggable) ? (
         <Draggable key={card.id}>{cardToRender}</Draggable>
       ) : (
-        <span key={card.id}>{cardToRender}</span>
-      )
+          <span key={card.id}>{cardToRender}</span>
+        )
     })
 
     return (
@@ -179,8 +179,8 @@ class Lane extends Component {
           dragClass={cardDragClass}
           onDragStart={this.onDragStart}
           onDrop={e => this.onDragEnd(id, e)}
-          onDragEnter={() => this.setState({isDraggingOver: true})}
-          onDragLeave={() => this.setState({isDraggingOver: false})}
+          onDragEnter={() => this.setState({ isDraggingOver: true })}
+          onDragLeave={() => this.setState({ isDraggingOver: false })}
           shouldAcceptDrop={this.shouldAcceptDrop}
           getChildPayload={index => this.props.getCardDetails(id, index)}>
           {cardList}
@@ -194,18 +194,18 @@ class Lane extends Component {
   }
 
   removeLane = () => {
-    const {id} = this.props
-    this.props.actions.removeLane({laneId: id})
+    const { id } = this.props
+    this.props.actions.removeLane({ laneId: id })
     this.props.onLaneDelete(id)
   }
 
   updateTitle = value => {
-    this.props.actions.updateLane({id: this.props.id, title: value})
-    this.props.onLaneUpdate(this.props.id, {title: value})
+    this.props.actions.updateLane({ id: this.props.id, title: value })
+    this.props.onLaneUpdate(this.props.id, { title: value })
   }
 
   renderHeader = (pickedProps) => {
-    const {components} = this.props
+    const { components } = this.props
     return (
       <components.LaneHeader
         {...pickedProps}
@@ -217,11 +217,11 @@ class Lane extends Component {
   }
 
   toggleLaneCollapsed = () => {
-    this.props.collapsibleLanes && this.setState(state => ({collapsed: !state.collapsed}))
+    this.props.collapsibleLanes && this.setState(state => ({ collapsed: !state.collapsed }))
   }
 
   render() {
-    const {loading, isDraggingOver, collapsed} = this.state
+    const { loading, isDraggingOver, collapsed } = this.state
     const {
       id,
       cards,
@@ -238,7 +238,7 @@ class Lane extends Component {
       ...otherProps
     } = this.props
     const allClassNames = classNames('react-trello-lane', this.props.className || '')
-    const showFooter = collapsibleLanes && cards.length > 0
+    const showFooter = true
     return (
       <components.Section
         {...otherProps}
@@ -246,7 +246,7 @@ class Lane extends Component {
         onClick={() => onLaneClick && onLaneClick(id)}
         draggable={false}
         className={allClassNames}>
-        {this.renderHeader({id, cards, ...otherProps})}
+        {this.renderHeader({ id, cards, ...otherProps })}
         {this.renderDragContainer(isDraggingOver)}
         {loading && <components.Loader />}
         {showFooter && <components.LaneFooter onClick={this.toggleLaneCollapsed} collapsed={collapsed} />}
@@ -295,8 +295,8 @@ Lane.defaultProps = {
   labelStyle: {},
   label: undefined,
   editable: false,
-  onLaneUpdate: () => {},
-  onCardAdd: () => {}
+  onLaneUpdate: () => { },
+  onCardAdd: () => { }
 }
 
 const mapDispatchToProps = dispatch => ({
